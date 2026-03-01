01e316835d84a959f5673ced62b6ab24af244675
AMAN KUMAR SETH <amanthecoder04@gmail.com>
2026-03-01 21:14:23 +0530

    Java object mapper added in AnalyticsConsumer and NotificationConsumer for printing in JSON format.

--- order/src/main/java/com/aman/order/analytics/consumer/AnalyticsConsumer.java
+++ order/src/main/java/com/aman/order/analytics/consumer/AnalyticsConsumer.java
@@ -2,6 +2,8 @@ package com.aman.order.analytics.consumer;
 
 import com.aman.order.common.KafkaTopics;
 import com.aman.order.common.event.OrderCreatedEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -10,14 +12,15 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class AnalyticsConsumer {
 
+    private final ObjectMapper mapper = new ObjectMapper();
+
     @KafkaListener(
             topics = KafkaTopics.ORDER_EVENTS,
             groupId = "analytics-group"
     )
-    public void listen(OrderCreatedEvent event) {
-
-        log.info("[AnalyticsConsumer] Analytics received: {}", event);
-
-        log.info("[AnalyticsConsumer] Updating analytics for order {}", event.getOrderId());
+    public void listen(OrderCreatedEvent event) throws JsonProcessingException {
+        String json = mapper.writeValueAsString(event);
+        log.info("[AnalyticsConsumer][Analytics JSON received]: {}", json); // JSON Format
+        log.info("[AnalyticsConsumer][Analytics Java Object received]: {}", event); // Java Object Format
     }
 }
--- order/src/main/java/com/aman/order/notification/consumer/NotificationConsumer.java
+++ order/src/main/java/com/aman/order/notification/consumer/NotificationConsumer.java
@@ -2,6 +2,8 @@ package com.aman.order.notification.consumer;
 
 import com.aman.order.common.KafkaTopics;
 import com.aman.order.common.event.OrderCreatedEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -10,14 +12,15 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class NotificationConsumer {
 
+    private final ObjectMapper mapper = new ObjectMapper();
+
     @KafkaListener(
             topics = KafkaTopics.ORDER_EVENTS,
             groupId = "notification-group"
     )
-    public void listen(OrderCreatedEvent event) {
-
-        log.info("[NotificationConsumer] Notification received: {}", event);
-
-        log.info("[NotificationConsumer] Email sent for order {}", event.getOrderId());
+    public void listen(OrderCreatedEvent event) throws JsonProcessingException {
+        String json = mapper.writeValueAsString(event);
+        log.info("[NotificationConsumer][Notification Json received]: {}", json); // JSON Format
+        log.info("[NotificationConsumer][Notification Java Object received]: {}", event); // Java Object Format
     }
 }
